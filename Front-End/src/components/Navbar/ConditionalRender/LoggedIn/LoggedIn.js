05fb7a2b5551bae9040d4d143c1b88f3a9475d25
Aditya <pratapsinghaditya2001@gmail.com>
2020-10-22 16:24:02 +0530

    Added DropDowns in NavBar

--- Front-End/src/components/Navbar/ConditionalRender/LoggedIn/LoggedIn.js
+++ Front-End/src/components/Navbar/ConditionalRender/LoggedIn/LoggedIn.js
@@ -63,7 +63,78 @@ class LoggedIn extends Component {
           </NavLink>
         </li>
 
-        
+        {/* User dropdown */}
+        <li class="user nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <i class="fas fa-user-circle"></i>
+            <br></br>
+            <h6>Aditya</h6>
+          </a>
+
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <NavLink to='/user'>
+            <div className='navUserProfHead'>
+              <div className="navUserProfile">
+                {/* <i class="fas fa-2x fa-user-circle"></i> */}
+                <img className='navDropdownProfImg' src={profSample}/>
+              </div>
+              <div style={{display:'inline-block'}}>
+                <h6 className='name'>Aditya Pratap Singh</h6>
+                <h6 className='status'>Web Developer at Software Incubator (SDC-SI)</h6>
+              </div>
+            </div>
+            <button className='viewProfile'>View Profile</button>
+            </NavLink>
+            <div className='dropAccount'>
+              <h6>ACCOUNT</h6>
+            </div>
+            <NavLink to='/user'>
+              <h6 className='dropAccountOptions'>Settings & Privacy</h6>
+            </NavLink>
+            <NavLink to='/postedJobs'>
+              <h6 className='dropAccountOptions'>My Posted Jobs</h6>
+            </NavLink>
+            <div className='dropAccount'>
+              <h6>MANAGE</h6>
+            </div>
+            <h6 className='dropAccountOptions'>SIGN OUT</h6>
+          </div>
+        </li>
+
+        <li class="user nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <i class="fas fa-th"></i>
+            <br></br>
+            <h6>Work</h6>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <div className='dropAccount'>
+              <h6>CREATE</h6>
+            </div>
+            <NavLink to='/postJob'>
+              <h6 className='dropAccountOptions'>Post a job</h6>
+            </NavLink>
+            <NavLink to='/createPage'>
+              <h6 className='dropAccountOptions'>Create Company page +</h6>
+            </NavLink>
+          </div>
+        </li>
       </ul>
     );
   }
